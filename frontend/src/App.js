a343526d4e7fef59e989cbb3474d1232bd740662
leandro-web <leandro-web@hotmail.com>
2020-03-25 20:19:39 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React, { useState } from 'react';
+//import Header from './Header';
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Hello World OmniStack
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+	return(
+		<Routes />
 	);
+
+	/*const [counter, setCounter]  = useState(0);
+	function increment(){
+		setCounter(counter + 1);
+	}
+	return (
+		<div>
+			<Header title="Semana OmniStack" >Contador: {counter}</Header>
+			<button onClick={increment}>Incrementar</button>
+		</div>
+	);*/
 }
 
 export default App;
